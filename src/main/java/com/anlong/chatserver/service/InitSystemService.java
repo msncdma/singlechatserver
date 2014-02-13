ac69c8dd70b93b38a414ab05e24deed6dd14c4fb
lixl <msncdma@foxmail.com>
2014-02-13 17:52:41 +0800

    init server first

---
+++ src/main/java/com/anlong/chatserver/service/InitSystemService.java
@@ -0,0 +1,19 @@
+package com.anlong.chatserver.service;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+
+public class InitSystemService implements ServletContextListener {
+
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
